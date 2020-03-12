65782e12cb6227398bce8521c67945b84ff47e95
Minoru Edo <minoruedo@MinorunoMacBook-Pro.local>
2020-03-12 22:37:53 +0900

    8-9_routesの設定と基本のviewファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'tops#index'    
+
+  get 'answers/index'
+  get 'answers/new'
+  
+  resources :stockplains
+  resources :prefs
+  resources :genders
+  resources :jobs
+  resources :ages
+  
+  resources :answers
+
+  resources :tops
+
+  
 end
